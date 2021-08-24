509d654c4fedda31676e858427386e44e2346f89
adamilnicki <adamilnicki00@gmail.com>
2021-08-24 14:41:06 +0200

    color changer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
-    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
+let button = document.getElementById("btn");
+let body = document.querySelector("body");
+let span = document.getElementById("hex-value");
+let hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
-    button.addEventListener('click', changeHex)
-
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
-        }
-        value.textContent = hex
-        body.style.backgroundColor = hex
+button.addEventListener('click',function colorChange(){
+    //Generate hex
+    let hexColor = [];
+    for(let i=0;i<6;i++){
+        let ran = Math.ceil(Math.random()*hex.length-1);
+        let newHex = hex[ran];
+        hexColor.push(newHex);
     }
-} )()
+    body.style.backgroundColor = `#${hexColor[0]}${hexColor[1]}${hexColor[2]}${hexColor[3]}${hexColor[4]}${hexColor[5]}`;
+    span.innerHTML = `#${hexColor[0]}${hexColor[1]}${hexColor[2]}${hexColor[3]}${hexColor[4]}${hexColor[5]}`;
+})
